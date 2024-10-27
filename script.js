95c5c926b57b1c7278784d31c31d7aa106d3da66
koushik16 <63333977+koushik16@users.noreply.github.com>
2024-10-27 03:30:59 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth Scrolling for Sidebar Links
+document.querySelectorAll('.side-nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
